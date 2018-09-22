9215b68d87f2ba132f994c7d2c724799a2080d02
garrammb <garrammb@clarkson.edu>
2018-09-21 22:25:03 -0400

    more setup

--- app/src/main/java/com/example/mike/easymarket/login.java
+++ app/src/main/java/com/example/mike/easymarket/login.java
@@ -2,6 +2,8 @@ package com.example.mike.easymarket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class login extends AppCompatActivity {
 
@@ -10,4 +12,28 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.market_login);
     }
+
+
+    private void attemptLogin(String username, String password) {
+        if (isInDb(username)) {
+            if (correctCreds(username, password)) {
+
+            } else {
+                android.widget.Toast.makeText(getApplicationContext(), "incorrect password", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            android.widget.Toast.makeText(getApplicationContext(), username +
+                    "is not a valid username", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isInDb(String username) {
+        return false;
+    }
+
+    private boolean correctCreds(String username, String password) {
+        return false;
+    }
+
+
 }
